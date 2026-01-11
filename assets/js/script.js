1bae642a22692fd0554318437514fe17b1d99642
FabryPostRock <fabriziodemasi01@gmail.com>
2026-01-11 10:47:50 +0100

    fixed thumb reaction not starting

--- assets/js/script.js
+++ assets/js/script.js
@@ -64,16 +64,12 @@ function openGenderSelModal() {
     );
     closeButtons.forEach((btn) => btn.setAttribute("disabled", true));
     mod.addEventListener("hide.bs.modal", () => {
-      console.log("removing focus from modal!");
       const active = document.activeElement;
-      console.log(active);
       if (active && mod.contains(active)) {
         active.blur();
       }
-      document.querySelector("#h1-procastination")?.focus();
     });
     mod.addEventListener("hidden.bs.modal", () => {
-      console.log("new focus on h1!");
       document.querySelector("#h1-procastination")?.focus();
     });
 
@@ -137,24 +133,20 @@ document.querySelectorAll(".btn-reaction").forEach((btn) => {
   });
 });
 
-function animationStart(event, span, type) {
+function spawnReaction(btn, type) {
+  const span = document.createElement("span");
   // imposto a hidden altrimenti potrebbe comparire un elemento all'interno del pulsante con font grande come il like se non venisse consumato subito dall'animazione.
-  span.style.display = "none";
+  //span.style.display = "none";
   // Google Font Icon Version
   span.className =
     "material-symbols-outlined g-icon-secondary-color floating-reaction";
   span.textContent = type === "up" ? "thumb_up" : "thumb_down";
-}
-
-function spawnReaction(buttonEl, type) {
-  const span = document.createElement("span");
-
   // random live-like
   const drift = rand(-18, 18); // spostamento laterale
   const rise = rand(90, 160); // quanto sale
   const rot = rand(-18, 18); // rotazione
   const dur = rand(700, 1100); // durata
-  const scale = rand(0.95, 1.25); // scala finale
+  const scale = rand(0.75, 1.25); // scala finale
 
   // creaimo le proprietà che poi verranno richiamate in '@keyframes floatUp'
   span.style.setProperty("--drift", `${drift}px`);
@@ -163,10 +155,10 @@ function spawnReaction(buttonEl, type) {
   span.style.setProperty("--dur", `${dur}ms`);
   span.style.setProperty("--scale", scale);
 
-  span.addEventListener("animationstart", (event) =>
-    animationStart(event, span, type)
-  );
-  buttonEl.appendChild(span);
+  span.addEventListener("animationstart", () => {
+    console.log("animationstart fired", getComputedStyle(span).animationName);
+  });
+  btn.appendChild(span);
   // pulizia a fine animazione
   span.addEventListener("animationend", () => span.remove());
 }
